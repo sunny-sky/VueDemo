25a0eb3d3a68653cf2ddccd93137a6474055af5f
sunny-sky <422385795@qq.com>
2018-07-30 21:04:28 +0800

    eslint

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,13 +5,13 @@
 </template>
 
 <script>
-  export default {
-    data(){
-      return{
-        msg:'Hello Vue Component.This is LH\'s first Vue project'
-      }
+export default {
+  data () {
+    return {
+      msg: 'Hello Vue Component.This is LH\'s first Vue project'
     }
   }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
