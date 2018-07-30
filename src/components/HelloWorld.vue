367b259ae8753f63fd8cc8fab6f908bc9e84dbdb
sunny-sky <422385795@qq.com>
2018-07-30 20:37:00 +0800

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p class="msg">{{msg}}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return{
+        msg:'Hello Vue Component.This is LH\'s first Vue project'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+  .msg{
+    color:red;
+    fdnt-size: 30px;
+  }
+</style>
