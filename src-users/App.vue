fce2c0827791984828f75dc9668248b40344a33c
sunny-sky <422385795@qq.com>
2018-08-11 22:50:55 +0800

    20180811: No. 38

---
+++ src-users/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <Search></Search>
+    <users-main></users-main>
+  </div>
+</template>
+
+<script>
+import Search from './components/Search'
+import Main from './components/Main'
+export default {
+  components: {
+    Search,
+    UsersMain: Main
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+.card {
+  float:left;
+  width: 33.333%;
+  padding: .75rem;
+  margin-bottom: 2rem;
+  border: 1px solid #efefef;
+  text-align: center;
+}
+
+.card > img {
+  margin-bottom: .75rem;
+  border-radius: 100px;
+}
+
+.card-text {
+  font-size: 85%;
+}
+</style>
--- src-users/main.js
+++ src-users/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
+import VueResource from 'vue-resource'
 import App from './App.vue'
-import {Button} from 'mint-ui'
 
-// 注册成标签（全局）
-Vue.component(Button.name, Button)
+// 声明使用插件
+Vue.use(VueResource) // 内部会给vm对象和组件对象添加一个属性： $http
 
 /* eslint-disable no-new */
 new Vue({
